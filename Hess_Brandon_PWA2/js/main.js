45d77721c911ac67ba9f12e3ef6240a69ce3624b
BHess2653 <GM_Dahaka@Brandons-MacBook-Pro-2.local>
2015-04-17 23:32:10 -0400

    Commit 5

--- Hess_Brandon_PWA2/js/main.js
+++ Hess_Brandon_PWA2/js/main.js
@@ -4,11 +4,241 @@
 */
 
 (function($){
-	
-	
-	
-	
-		
+
+
+/* ============================== Tooltip =============================== */
+
+$('.masterTooltip').hover(function(){
+	// Hover over code
+	var title = $(this).attr('title');
+	$(this).data('tipText', title).removeAttr('title');
+	$('<p class="tooltip"></p>')
+	.text(title)
+	.appendTo('body')
+	.fadeIn('slow');
+}, function() {
+	//  Hover out code
+	$(this).attr('title', $(this).data('tipText'));
+	$('.tooltip').remove();
+}).mousemove(function(e) {
+	var mousex = e.pageX + 20; // Get X coordnates
+	var mousey = e.pageY + 10; // Get Y coordnates
+	$('.tooltip')
+	.css({ top: mousey, left: mousex })
+});
+
+
+
+/* ============================== Login ================================== */
+
+$('#signinButton').click(function(){
+  	var user = $('#user').val();
+  	var pass = $('#pass').val();
+  	console.log("This notifies you if the password is working");
+  	$.ajax({
+      url:'xhr/login.php',
+      type: 'post',
+      dataType: 'json',
+      data: {
+        	username: user,
+        	password: pass
+      },
+      success:function(response){
+        console.log("Test User");
+        if(response.error){
+          alert(response.error);
+        }else{
+          window.location.assign('admin.html')
+        };
+      }
+    });
+});
+
+/* ============================== Register ================================= */
+
+$('#register').on('click', function(){
+  	var firstname= $('#first').val(),
+        lastname= $('#last').val(),
+        username= $('#userName').val(),
+        email= $('#email').val(),
+        password= $('#password').val();
+  console.log(firstname+' '+lastname+' '+username+' '+email+' '+password);
+  
+  	$.ajax({
+      	url:'xhr/register.php',
+      	type: 'post',
+      	dataType: 'json',
+      	data: {
+      			firstname: firstname,
+      			lastname: lastname,
+          	username: username,
+          	email: email,
+          	password: password
+    		},
+      
+      	success: function(response){
+          	if (response.error){
+              	alert(response.error);
+            }else{
+              window.location.assign('admin.html');
+            }
+        }
+    });
+});
+
+/* ============================== Go to Project page ========================= */
+
+$('.projectsbtn').on('click', function(e) {
+  		e.preventDefault();
+  		window.location.assign('projects.html');
+});
+
+
+/* ============================== Add Modal ================================== */
+
+
+$('.modalClick').on('click', function(event){
+  	event.preventDefault();
+  	$('#overlay')
+    	.fadeIn()
+    	.find('#modal')
+    	.fadeIn();
+});
+
+$('.close').on('click', function(event){
+  	event.preventDefault();
+  	$('#overlay')
+    	.fadeOut()
+    	.find('#modal')
+    	.fadeOut();
+});
+
+/* ============================== Fading status Option ======================== */
+
+$('.mystatus').mouseover(function(){
+  	$(this).fadeTo(100, .3);
+});
+
+$('.mystatus').mouseout(function(){
+  	$(this).fadeTo(100, 1);
+});
+
+/* ============================== Template User ID ============================ */
+
+
+/* ================== Tabbed Accordion for Project ============================ */
+
+$('#tabs p').hide().eq(0).show();
+
+$('#tabs-nav li').click(function(e) {
+  e.preventDefault();
+    $('#tabs p').hide();
+  
+      $('#tabs-nav .current').removeClass("current");
+        $(this).addClass('current');
+          var clicked = $(this).find('a:first').attr('href');
+          
+            $('#tabs ' + clicked).fadeIn('fast');
+}).eq(0).addClass('current');
+
+
+
+
+
+
+
+/* ============================== Button hover sound ========================= */
+
+$("button").each(function(i) {if (i != 0) { 
+      		$("#hover")
+        	.clone()
+        	.attr("id", "hover" + i)
+        	.appendTo($(this).parent()); 
+    	}
+    		$(this).data("beeper", i);
+  		})
+  			.mouseenter(function() {
+    		$("#hover" + $(this).data("beeper"))[0].play();
+  		});
+			$("#hover").attr("id", "hover0");
+
+
+
+/* ============================== Button click sound ========================= */
+
+		$( document ).ready(function() {
+			var audio = $("#click")[0];
+			$("button").mousedown(function() {
+			  audio.play();
+			});
+		});
+
+
+/* =========================== Button close/cancel sound ===================== */
+
+		$( document ).ready(function() {
+			var audio = $("#cancel")[0];
+			$("span").mousedown(function() {
+			  audio.play();
+			});
+		});
+
+
+
+
+/* ============================== Konami code =============================== */
+
+(function() {
+  var app;
+
+  $(document).ready(function() {
+    return app.init();
+  });
+
+  app = {
+    code: "",
+    konami: "38384040373937396665",
+    pressed_key: 0,
+    init: function() {
+      return this.bind_events();
+    },
+    bind_events: function() {
+      return $(document).keydown(function(e) {
+        app.pressed_key = e.which || e.keyCode;
+        return app.update_code();
+      });
+    },
+    update_code: function() {
+      this.code += this.pressed_key;
+      return this.check_code();
+    },
+    check_code: function() {
+      if (this.code.length === 20) {
+        if (this.code === this.konami) {
+          this.easter_egg();
+          return this.code = "";
+        } else {
+          return this.code = this.code.substring(2);
+        }
+      }
+    },
+    easter_egg: function() {
+      $("html, body").css("height", "100%");
+      $("body").css({"background-image": "url(http://img1.wikia.nocookie.net/__cb20150406225110/freddy-fazbears-pizza/images/b/b2/SpringtrapJumpscareFromLeft.gif)", "z-index": "5"});
+      $("body").css("background-position", "center center");
+      $("body").css("background-repeat", "no-repeat");
+      $('#secret-found')[0].play(); // plays the sound
+      // return alert("That's how I roll baby <3");
+    }
+  };
+
+
+}).call(this);
+
+
+
+
+
 
 	
 })(jQuery); // end private scope
